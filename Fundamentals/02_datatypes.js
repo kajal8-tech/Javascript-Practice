b603c36f89f85b5131dae237d63088958503f1df
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-06-06 23:39:28 +0530

    datatypes and shortnotes

---
+++ Fundamentals/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all js code as newer version
+
+// alert(3+3) // We are using nodejs,not browser
+
+let name="Kajal"
+let age=18
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean =>true/false
+// null =>standalone value / Empty value
+// undefined => values not assigned
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
