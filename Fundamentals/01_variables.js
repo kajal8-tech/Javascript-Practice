9e9df7b34dc32bd97178a58928f6677e98c786d5
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-06-06 23:17:37 +0530

    variables in js with short notes

---
+++ Fundamentals/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=144553
+let accoutEmail= "kajal@gmail.com"
+var accountPassword= "12345"
+accountCity= "Odisha"
+let accountState
+
+// accountId=2 // not allowed
+
+accountEmail="kajal@sahu.com"
+accountPassword="213423"
+accountcity="Beengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+1.Prefer not to use var
+because of issues in block scope and functional scope
+2.In javascript which variables are assigned they are undefind
+*/
