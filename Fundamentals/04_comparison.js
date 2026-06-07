d8e61d56dfc96007c795b985337ed7f02df0a007
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-06-07 22:56:00 +0530

    How comparison works

---
+++ Fundamentals/04_comparison.js
@@ -0,0 +1,25 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+// same datatype
+
+console.log("2">1);
+console.log("02">1);
+// Diffrent datatype
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+// Null comparison
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+// Undefined comparison
+
+// strict check(===)
+console.log("2"===2);
+
+
