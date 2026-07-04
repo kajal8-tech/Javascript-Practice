44062a169f84576cbe88a95264aef96acc3ccea2
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-04 11:13:32 +0530

    Numbers in js

---
+++ Fundamentals/06_NumsAndMaths.js
@@ -0,0 +1,18 @@
+const score = 100
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber=123.8967
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
