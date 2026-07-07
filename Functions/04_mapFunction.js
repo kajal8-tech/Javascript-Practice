4fd13abc714e24411de50690648705fa09538ec2
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-07 21:21:08 +0530

    mapfunction

---
+++ Functions/04_mapFunction.js
@@ -0,0 +1,9 @@
+//Map function is similar to forEach but it returns the value in a new array
+let num=[1,2,3,4];
+
+let newArr=num.map((val)=>{
+    console.log(val*val);
+})
+console.log(newArr);
+
+
