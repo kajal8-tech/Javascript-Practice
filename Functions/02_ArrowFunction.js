b1c8ebcf979b0ff0b8e13981144bec3757b678fe
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-07 17:53:49 +0530

    Sum of two numbers using arrow function

---
+++ Functions/02_ArrowFunction.js
@@ -0,0 +1,8 @@
+//Arrow Function is the compact alternative to the traditional function expression with some difference
+
+//Sum of two numbers using arrow function
+const arrowSum=(a,b)=>{
+    console.log(a+b);
+}
+arrowSum(2,4);
+
